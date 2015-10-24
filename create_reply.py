ab78c59c67f19bc787dd8c9979abb3763772d75e
magicwifi <huangzhe1218@163.com>
2015-10-24 18:27:27 +0800

    10025

---
+++ create_reply.py
@@ -0,0 +1,32 @@
+#-*- coding:utf-8 -*-
+import requests
+import sys
+from sys import argv
+from bs4 import BeautifulSoup
+import urllib  
+import urllib2  
+
+def main(basic_case_id,doctor_id,sick_name):  
+	payload = {'sick_name':sick_name,'main_desc':'test'}
+	url = "http://117.34.78.201:8081/"+basic_case_id+"/"+doctor_id+"/create_reply"
+	headers = { "Accept":"text/html,application/xhtml+xml,application/xml;",
+            "Accept-Encoding":"gzip",
+            "Accept-Language":"zh-CN,zh;q=0.8",
+            "Referer":"http://www.example.com/",
+            "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
+            }
+	data = urllib.urlencode(payload)
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
+	result = response.read()
+	print result
+
+if __name__ == '__main__':  
+	if len(sys.argv) != 4:
+		print 'Usage: python input_name output_name'
+		exit(1)
+	basic_case_id = sys.argv[1]
+	doctor_id = sys.argv[2]
+	sick_name = sys.argv[3]
+	main(basic_case_id,doctor_id,sick_name) 
+
---
+++ update_doctor_url.py
@@ -0,0 +1,31 @@
+#-*- coding:utf-8 -*-
+import requests
+import sys
+from sys import argv
+from bs4 import BeautifulSoup
+import urllib  
+import urllib2  
+
+def main(doctor_id,url):  
+	payload = {'url':url}
+	url = "http://117.34.78.201:8081/"+doctor_id+"/update_doctor_url"
+	headers = { "Accept":"text/html,application/xhtml+xml,application/xml;",
+            "Accept-Encoding":"gzip",
+            "Accept-Language":"zh-CN,zh;q=0.8",
+            "Referer":"http://www.example.com/",
+            "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
+            }
+	data = urllib.urlencode(payload)
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
+	result = response.read()
+	print result
+
+if __name__ == '__main__':  
+	if len(sys.argv) != 3:
+		print 'Usage: python input_name output_name'
+		exit(1)
+	doctor_id = sys.argv[1]
+	url = sys.argv[2]
+	main(doctor_id,url) 
+
